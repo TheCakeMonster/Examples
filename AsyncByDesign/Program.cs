b3e11c475b3c5d45885ca419a76ab1228b5f0ea6
TheCakeMonster <46025270+TheCakeMonster@users.noreply.github.com>
2021-12-04 12:16:33 +0000

    Initial version - synchronous

---
+++ AsyncByDesign/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace AsyncByDesign;
+
+public class Program
+{ 
+	static void Main(string[] args)
+	{
+		IList<string> names;
+
+		// Get the data to display
+		Initialise();
+		names = GetNames();
+
+		// Use the data
+		foreach (string name in names)
+		{
+			Console.WriteLine(name);
+		}
+	}
+
+	private static void Initialise()
+	{
+		// TODO: Do intial setup work here
+	}
+
+	private static IList<string> GetNames()
+	{
+		IList<string> names;
+
+		names = new List<string>()
+		{
+			"Sue,",
+			"Joe",
+			"Alice"
+		};
+
+		return names;
+	}
+}
